58097288bac7418c3a732170d12d8137a4f8977a
nash <fj0rd@qq.com>
2025-05-29 09:59:08 +0800

    Add minijinja.rs sandbox script with template examples
    - Added a Minijinja sandbox script with template examples
    - Included dependencies and rendering logic for different template scenarios

---
+++ data/sandbox/minijinja.rs
@@ -0,0 +1,33 @@
+//! ```cargo
+//! [dependencies]
+//! serde_json = "1.0.140"
+//! minijinja = { version = "2.10.2", features = ["loader"] }
+//! ```
+
+use minijinja::{Environment, context};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut env = Environment::new();
+    env.add_template("hello.txt", "Hello {{ name }}!").unwrap();
+    let template = env.get_template("hello.txt").unwrap();
+    println!("{}", template.render(context! { name => "World" }).unwrap());
+
+    let a = r#"
+        {% raw %}
+            <ul>
+            {% for item in seq %}
+                <li>{{ item }}</li>
+            {% endfor %}
+            </ul>
+        {% endraw %}
+    "#;
+    env.add_template("a", a).unwrap();
+    println!("{}", env.get_template("a").unwrap().render(context!{}).unwrap());
+
+    let b = r#"{% for i in range(rank - 1) -%} {{ "{}," -}} {% endfor %}"#;
+    env.add_template("b", b).unwrap();
+    println!("{}", env.get_template("b").unwrap().render(context!{ rank => 6 }).unwrap());
+
+
+    Ok(())
+}
